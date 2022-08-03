439d8e70af0b6fc52f3daae327ecefa233f6e018
Oliver Barnwell <barnwelloliver@gmail.com>
2022-08-03 02:24:36 +0100

    feat: add a nice drop shadow

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -23,12 +23,10 @@ const Timeline = () => {
       {currentWeek.map((date) => {
         return (
           <Day date={date}>
-            <input></input>
             <p>
               In publishing and graphic design, Lorem ipsum is a placeholder
               text commonly used to demonstrate the visual form of a document or
-              a typeface without relying on meaningful content. Lorem ipsum may
-              be used as a placeholder before final copy is available.
+              a typeface without relying on meaningful content.
             </p>
           </Day>
         );
