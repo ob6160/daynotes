904767cebeb136f19db968039eef0d405bd3c966
Oliver Barnwell <barnwelloliver@gmail.com>
2022-08-04 23:48:07 +0100

    chore: prettier and alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Preact to support Preact JSX components.
   integrations: [preact()],
   // Enable SSR on Vercel.
-  output: "server",
+  output: 'server',
   adapter: vercel(),
   vite: {
     ssr: {
-      noExternal: ["modern-css-reset"],
+      noExternal: ['modern-css-reset'],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
   },
 });
