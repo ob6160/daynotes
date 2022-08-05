2341db826c332d2edb924e9e04b0b17efe5a3172
Oliver Barnwell <barnwelloliver@gmail.com>
2022-08-05 17:06:26 +0100

    feat: more work on button styling, text input and note logic

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'preact';
+import { useCallback, useState } from 'preact/hooks';
 import './Day.scss';
 
 interface Props {
@@ -21,29 +22,63 @@ const Title: FunctionComponent<{ date: Date }> = ({ date }) => {
         <h2>{dayPart}</h2>
         <p class="tagline">{`${monthPart} ${yearPart}`}</p>
       </section>
-      <section class="mood"></section>
+    </section>
+  );
+};
+
+type Note = {
+  title?: string;
+  content?: string;
+};
+
+const Note: FunctionComponent<Note> = ({ content }) => {
+  return (
+    <section class="note">
+      <textarea
+        placeholder="Write in me!"
+        value={content}
+      />
+      <button class="clear">
+        <i class="fa-solid fa-close"></i>
+      </button>
     </section>
   );
 };
 
 const Day: FunctionComponent<Props> = ({ date, children }) => {
-  const inputCount = [0];
+  const [notes, setNotes] = useState<Note[]>([]);
+  // const [images, setImages] = useState([]);
+  // const [links, setLinks] = useState([]);
+  // const [books, setBooks] = useState([]);
+  // const [music, setMusic] = useState([]);
+  const addNote = useCallback(() => {
+    setNotes([
+      ...notes,
+      {
+        title: 'Test',
+        content: 'test content...',
+      },
+    ]);
+  }, [notes]);
+
   return (
     <li class="day">
       <section>
         <Title date={date} />
         <section class="content">
-          {inputCount.map(() => (
-            // <textarea
-            //   class="note"
-            //   placeholder="Write in me!"
-            // />
-            <></>
+          {notes.map(({ title, content }) => (
+            <Note
+              title={title}
+              content={content}
+            />
           ))}
           {children}
         </section>
         <section class="controls">
-          <button class="action">
+          <button
+            class="action"
+            onClick={addNote}
+          >
             <i class="fa-solid fa-note-sticky"></i>
           </button>
           <button class="action">
